60bdce28976000701705224342472c2a28d398c9
Denis <gorokhov@gmx.com>
2019-02-25 11:46:03 +0300

    add try except

--- 3bot_geo/3bot_geo.py
+++ 3bot_geo/3bot_geo.py
@@ -8,18 +8,18 @@ from socket import timeout
 import os
 import redis
 def link():
-    req = Request('https://www.nationalgeographic.com/', headers={'User-Agent': 'Mozilla/62.0'})
-    time.sleep(2)
-    try:
-        page=urlopen(req,timeout=10).read()
+        req = Request('https://www.nationalgeographic.com/', headers={'User-Agent': 'Mozilla/62.0'})
         time.sleep(2)
-        soup = BeautifulSoup(page,"html.parser")
-        time.sleep(1)
-        data = json.loads(soup.find('script', type='text/json').find_next('script', type='text/json').text)
-        link = (data["body"][5]['magazine_package']["story_details"]['lead_story_details'][0]['uri'])
-        return link
-    except Exception:
-        pass
+        try:
+                page=urlopen(req,timeout=10).read()
+                time.sleep(2)
+                soup = BeautifulSoup(page,"html.parser")
+                time.sleep(1)
+                data = json.loads(soup.find('script', type='text/json').find_next('script', type='text/json').text)
+                link = (data["body"][5]['magazine_package']["story_details"]['lead_story_details'][0]['uri'])
+                return link
+        except Exception:
+                pass
 def post():
 	try:
 		BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
@@ -38,10 +38,13 @@ def getlastpost():
         msg = r.get("3geo:last")
         return msg
 def setlastpost():
-        redis_host = "redis-server"
-        redis_port = 6379
-        r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-        r.set("3geo:last", link())		
+        try:
+                redis_host = "redis-server"
+                redis_port = 6379
+                r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+                r.set("3geo:last", link())
+        except Exception:
+                pass
 while True:
 	if link() == getlastpost():
 		print("The url is already posted")
--- insta/code.py
+++ insta/code.py
@@ -9,30 +9,30 @@ import re
 import os
 import redis
 def link():
-    try:
-        req = Request('https://www.instagram.com/natgeo/', headers={'User-Agent': 'Mozilla/62.0'})
-        time.sleep(2)
-        page=urlopen(req,timeout=10).read()
-        soup = BeautifulSoup(page,"html.parser")
-        data = soup.find('script', type='text/javascript').find_next('script', type='text/javascript').find_next('script', type='text/javascript').find_next('script', type='text/javascript').text
-        time.sleep(1)
-        json_data=json.loads(data[21:-1])
-        linktmp = (json_data['entry_data']['ProfilePage'][0]["graphql"]['user']['edge_owner_to_timeline_media']['edges'][0]['node']['shortcode'])
-        link='https://www.instagram.com/p/'+linktmp
-        return link
-    except Exception:
-        pass
+        try:
+                req = Request('https://www.instagram.com/natgeo/', headers={'User-Agent': 'Mozilla/62.0'})
+                time.sleep(2)
+                page=urlopen(req,timeout=10).read()
+                soup = BeautifulSoup(page,"html.parser")
+                data = soup.find('script', type='text/javascript').find_next('script', type='text/javascript').find_next('script', type='text/javascript').find_next('script', type='text/javascript').text
+                time.sleep(1)
+                json_data=json.loads(data[21:-1])
+                linktmp = (json_data['entry_data']['ProfilePage'][0]["graphql"]['user']['edge_owner_to_timeline_media']['edges'][0]['node']['shortcode'])
+                link='https://www.instagram.com/p/'+linktmp
+                return link
+        except Exception:
+                pass
 def post():
-    try:
-        BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-        CHANNEL_NAME = os.environ.get("CHANNEL_NAME", None)
-        time.sleep(1)
-        bot = telebot.TeleBot(BOT_TOKEN)
-        time.sleep(1)
-        bot.send_message(CHANNEL_NAME, link())
-        time.sleep(1)
-    except Exception:
-        pass
+        try:
+                BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+                CHANNEL_NAME = os.environ.get("CHANNEL_NAME", None)
+                time.sleep(1)
+                bot = telebot.TeleBot(BOT_TOKEN)
+                time.sleep(1)
+                bot.send_message(CHANNEL_NAME, link())
+                time.sleep(1)
+        except Exception:
+                pass
 def getlastpost():
         redis_host = "redis-server"
         redis_port = 6379
@@ -40,10 +40,13 @@ def getlastpost():
         msg = r.get("insta:last")
         return msg
 def setlastpost():
-        redis_host = "redis-server"
-        redis_port = 6379
-        r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-        r.set("insta:last", link())		
+        try:
+                redis_host = "redis-server"
+                redis_port = 6379
+                r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+                r.set("insta:last", link())
+        except Exception:
+                pass
 while True:
 	if link() == getlastpost():
 		print("The url is already posted")
