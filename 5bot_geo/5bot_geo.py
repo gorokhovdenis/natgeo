9504c8257b1fc5166d7e64abf56e4eb3b9aa96b6
Denis <gorokhov@gmx.com>
2019-02-25 11:27:13 +0300

    fix redis write error

--- 5bot_geo/5bot_geo.py
+++ 5bot_geo/5bot_geo.py
@@ -8,29 +8,29 @@ from socket import timeout
 import os
 import redis
 def link():
-    req = Request('https://www.nationalgeographic.com/', headers={'User-Agent': 'Mozilla/62.0'})
-    time.sleep(2)
-    try:
-        page=urlopen(req,timeout=10).read()
+        req = Request('https://www.nationalgeographic.com/', headers={'User-Agent': 'Mozilla/62.0'})
         time.sleep(2)
-        soup = BeautifulSoup(page,"html.parser")
-        time.sleep(1)
-        data = json.loads(soup.find('script', type='text/json').find_next('script', type='text/json').text)
-        link = (data["body"][5]['magazine_package']['sub_story_details']['sub_stories'][2]['uri'])
-        return link
-    except Exception:
-        pass
+        try:
+                page=urlopen(req,timeout=10).read()
+                time.sleep(2)
+                soup = BeautifulSoup(page,"html.parser")
+                time.sleep(1)
+                data = json.loads(soup.find('script', type='text/json').find_next('script', type='text/json').text)
+                link = (data["body"][5]['magazine_package']['sub_story_details']['sub_stories'][2]['uri'])
+                return link
+        except Exception:
+                pass
 def post():
-    try:
-        BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-        CHANNEL_NAME = os.environ.get("CHANNEL_NAME", None)
-        time.sleep(1)
-        bot = telebot.TeleBot(BOT_TOKEN)
-        time.sleep(1)
-        bot.send_message(CHANNEL_NAME, link())
-        time.sleep(1)
-    except Exception:
-        pass
+        try:
+                BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+                CHANNEL_NAME = os.environ.get("CHANNEL_NAME", None)
+                time.sleep(1)
+                bot = telebot.TeleBot(BOT_TOKEN)
+                time.sleep(1)
+                bot.send_message(CHANNEL_NAME, link())
+                time.sleep(1)
+        except Exception:
+                pass
 def getlastpost():
         redis_host = "redis-server"
         redis_port = 6379
@@ -38,10 +38,13 @@ def getlastpost():
         msg = r.get("5geo:last")
         return msg
 def setlastpost():
-        redis_host = "redis-server"
-        redis_port = 6379
-        r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-        r.set("5geo:last", link())		
+        try:
+                redis_host = "redis-server"
+                redis_port = 6379
+                r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+                r.set("5geo:last", link())
+        except Exception:
+                pass
 while True:
 	if link() == getlastpost():
 		print("The url is already posted")
