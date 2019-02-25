aa3731f4c151381996db2a3dcabf23864461acde
Denis <gorokhov@gmx.com>
2019-02-25 09:07:07 +0300

    fix

--- 1bot_geo/bot_geo.py
+++ 1bot_geo/bot_geo.py
@@ -9,18 +9,18 @@ import os
 import redis
 #test
 def link():
-    req = Request('https://www.nationalgeographic.com/', headers={'User-Agent': 'Mozilla/62.0'})
-    time.sleep(2)
-    try:
-        page=urlopen(req,timeout=10).read()
-        time.sleep(2)
-        soup = BeautifulSoup(page,"html.parser")
-        time.sleep(1)
-        data = json.loads(soup.find('script', type='text/json').find_next('script', type='text/json').text)
-        link = (data["body"][0]['homepage_package']["cards"][0]['uri'])
-        return link
-    except Exception:
-        pass
+        try:
+                req = Request('https://www.nationalgeographic.com/', headers={'User-Agent': 'Mozilla/62.0'})
+                time.sleep(2)
+                page=urlopen(req,timeout=10).read()
+                time.sleep(2)
+                soup = BeautifulSoup(page,"html.parser")
+                time.sleep(1)
+                data = json.loads(soup.find('script', type='text/json').find_next('script', type='text/json').text)
+                link = (data["body"][0]['homepage_package']["cards"][0]['uri'])
+                return link
+        except Exception:
+                pass
 def post():
 	try:
 		BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
@@ -39,10 +39,13 @@ def getlastpost():
         msg = r.get("1geo:last")
         return msg
 def setlastpost():
-        redis_host = "redis-server"
-        redis_port = 6379
-        r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-        r.set("1geo:last", link())		
+        try:
+                redis_host = "redis-server"
+                redis_port = 6379
+                r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+                r.set("1geo:last", link())
+        except Exception:
+                pass
 while True:
 	if link() == getlastpost():
 		print("The url is already posted")
@@ -53,4 +56,3 @@ while True:
 		post()
 		setlastpost()
 		print (link())
-
